da669993a1ef37b9420a8f596932addae60f44ac
HadiaShahzadi <hadiashahzadi@gmail.com>
2020-06-01 18:09:33 +0500

    abc

--- schools/settings.py
+++ schools/settings.py
@@ -136,3 +136,9 @@ import django_heroku
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
